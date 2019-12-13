a4114955c52325ddb432295aaff801f969b86084
vien pham <vienpham2019@gmail.com>
2019-12-13 22:04:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,6 @@
-reverse_each_word(sentence1)
+def reverse_each_word(sentence1)
+  array = sentence1.split(" ")
+  new_array = []
+  array.each {|element| new_array << element.reverse } 
+  return new_array.join
+end
