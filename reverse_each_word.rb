1df0b6372f8aed909a1216d7cb9029f16863b637
vien pham <vienpham2019@gmail.com>
2019-12-13 21:39:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,4 @@ def reverse_each_world(string)
   yield(string.split(" "))
 end
 
-reverse_each_world(string).each {|element| "#{element.reverse}"}
+reverse_each_world().each {|element| "#{element.reverse}"}
