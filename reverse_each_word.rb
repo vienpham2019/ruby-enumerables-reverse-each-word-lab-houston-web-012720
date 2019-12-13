04bd58a2ad475ea1915091806505bb4d6dff36b7
vien pham <vienpham2019@gmail.com>
2019-12-13 22:08:41 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(sentence1)
+  array = sentence1.split(" ")
+  new_array = []
+  array.each {|element| new_array << element.reverse } 
+  return new_array.join(" ")
+end
+
+def reverse_each_word(sentence1)
+  array = sentence1.split(" ")
+  new_array = []
+  array.collect {|element| new_array << element.reverse } 
+  return new_array.join(" ")
+end
