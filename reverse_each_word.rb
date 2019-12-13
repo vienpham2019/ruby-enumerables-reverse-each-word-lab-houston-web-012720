0593f72c384ed6576f86d3084955efae91496ea8
vien pham <vienpham2019@gmail.com>
2019-12-13 21:43:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,4 @@ def reverse_each_word(sentence1)
   yield(sentence1.split(" "))
 end
 
-reverse_each_word("Hello").each {|element| "#{element.reverse}"}
+reverse_each_word("Hello there, and how are you?").each {|element| "#{element.reverse}"}
